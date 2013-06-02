d2ddd2e67020ba2c4240ede34aec96bea9ebd6e1
psirenny <djtorres0@gmail.com>
2013-06-02 11:31:05 -0400

    allow custom token value useful for generating short tokens using the 'store' method

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ module.exports = function (options) {
   _.defaults(options, {
       expiration: 3600000
     , hashAlgorithm: 'sha1'
-    , mode: 'hash'
+    , method: 'hash'
   });
 
   if (!options.secretKey) {
     throw 'must provide a secretKey';
   }
 
-  if (options.mode !== 'hash') {
+  if (options.method !== 'hash') {
     if (!options.load) throw 'must provide a load() function';
     if (!options.save) throw 'must provide a save() function';
   }
 
-  var getHash = function (id, date) {
+  var getHash = function (id, value, date) {
+    value = value || [];
     if (!_.isArray(id)) id = _.isObject(id) ? _.values(id) : [id];
-    id = _.chain(id).push(date).join('').value();
-    return crypto.createHmac(options.hashAlgorithm, options.secretKey).update(id).digest('hex');
+    if (!_.isArray(value)) value = _.isObject(value) ? _.values(value) : [value];
+    var str = _.chain(id).concat(value).push(date).join('').value();
+    return crypto.createHmac(options.hashAlgorithm, options.secretKey).update(str).digest('hex');
   };
 
   return {
@@ -32,9 +34,9 @@ module.exports = function (options) {
         return elapsed > options.expiration;
       };
 
-      if (options.mode === 'hash') {
+      if (options.method === 'hash') {
         if (isExpired(token)) return callback('token expired');
-        return callback(token.hash === getHash(id, token.date) ? null : 'token invalid');
+        return callback(token.hash === getHash(id, token.value, token.date) ? null : 'token invalid');
       }
 
       if (arguments.length < 3) {
@@ -46,20 +48,24 @@ module.exports = function (options) {
         if (err) return callback(err);
         if (!dbToken) return callback('token not found');
         if (isExpired(dbToken)) return callback('token expired');
-        if (token) return callback(token.hash === dbToken.hash ? null : 'token invalid');
-        callback(dbToken.hash === getHash(id, dbToken.date) ? null : 'token invalid');
+        if (token.hash) return callback(dbToken.hash === token.hash ? null : 'token invalid');
+        return callback(dbToken.hash === getHash(id, token.value, token.date) ? null : 'token invalid');
       });
     }
-    , create: function (id, callback) {
+    , create: function (id, value, callback) {
       var date = +new Date()
-        , hash = getHash(id, date)
-        , token = {hash: hash, date: date};
+        , hash = getHash(id, value, date)
+        , token = {date: date, hash: hash}
+        , dbToken = {date: date, hash: hash}
 
-      if (options.mode === 'hash') {
+      if (value) token.value = value;
+
+      if (options.method === 'hash') {
         return callback(null, token);
       }
 
-      options.save(id, token, function (err) {
+      options.save(id, dbToken, function (err) {
+        if (value) delete token.hash;
         callback(err, token);
       });
     }
